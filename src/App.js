9d11e8ed0811bcc2f3a5a5d320f5393de543b9e5
Juan Sebastian Cardona <js.cardona12@uniandes.edu.co>
2017-03-09 09:31:56 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 
+const ROOT_URL = "localhost:9000/flickr/"
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          <h2>Flickr Rainbow</h2>
-            <div className="inline">
-            <p > Search for something on Flickr and we will get you a rainbow of it</p>
-                <a href="https://jscardona12.github.io/MyWebPage/ " > by Juan Sebastian Cardona </a>
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            fotosAzul: [],
+            fotosRojo:[],
+            fotosNaranja:[],
+            fotosAmarillo:[],
+            fotosVerde:[],
+            fotosMorado:[],
+            busqueda: '',
+            codigo: '',
+            nota: ''
+        }
+    }
+    obtenerFotos() {
+        axios.get(ROOT_URL+ this.state.busqueda + " blue")
+            .then(response => {
+                this.setState({
+                    fotosAzul: response.data
+                })
+            })
+        axios.get(ROOT_URL+ this.state.busqueda + " red")
+            .then(response => {
+                this.setState({
+                    fotosRojo: response.data
+                })
+            })
+        axios.get(ROOT_URL+ this.state.busqueda + " yellow")
+            .then(response => {
+                this.setState({
+                    fotosAmarillo: response.data
+                })
+            })
+        axios.get(ROOT_URL+ this.state.busqueda + " green")
+            .then(response => {
+                this.setState({
+                    fotosVerde: response.data
+                })
+            })
+        axios.get(ROOT_URL+ this.state.busqueda + " orange")
+            .then(response => {
+                this.setState({
+                    fotosNaranja: response.data
+                })
+            })
+    }
+    obtenerFotosRojo() {
+        axios.get(ROOT_URL+ this.state.busqueda + " purple")
+            .then(response => {
+                this.setState({
+                    fotosMorado: response.data
+                })
+            })
+    }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    {/*<img src={logo} className="App-logo" alt="logo" />*/}
+                    <h2>Flickr Rainbow</h2>
+                    <div className="inline">
+                        <p > Search for something on Flickr and we will get you a rainbow of it</p>
+                        <a href="https://jscardona12.github.io/MyWebPage/ " > by Juan Sebastian Cardona </a>
+                    </div>
+                    <input type="text" value={this.state.busqueda} onChange={(event) => {
+                        this.setState({busqueda: event.target.value})
+                    }}/>
+                </div>
+
             </div>
-            <input type="text"> </input>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
